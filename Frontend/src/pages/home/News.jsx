a27163c98f739829f5084ec76a62d884d350ae0a
SadMark011 <97679783+SadMark011@users.noreply.github.com>
2024-11-15 13:25:57 +0530

    Added Footer to the website Changed title of website

--- Frontend/src/pages/home/News.jsx
+++ Frontend/src/pages/home/News.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+// Import Swiper React components: Used To build responsive sliding components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Navigation} from 'swiper/modules';
+
 import news1 from '../../assets/news/news-1.png'
 import news2 from '../../assets/news/news-2.png'
 import news3 from '../../assets/news/news-3.png'
 import news4 from '../../assets/news/news-4.png'
+
 
 const news = [
     {
@@ -41,6 +53,57 @@ const News = () => {
   return (
     <div className='py-16'>
         <h2 className='text-3xl font-semibold mb-6'>News</h2>
+
+        <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+
+        navigation={true}
+
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+
+        modules={[Navigation]}
+        className="mySwiper" >
+
+            {
+                news.map((item,index) => (
+                    <SwiperSlide key={index}>
+                        <div className='flex flex-col sm:flex-row sm:justify-between items-center gap-12'>
+                            {/* Content */}
+                            <div className='py-4'>
+                                <Link to="/">
+                                    <h3 className='text-lg font-medium hover:text-blue-500 mb-4'>{item.title}</h3>
+                                </Link>
+
+                                <div className='w-12 bg-primary h-[4px] mb-5'></div>
+
+                                <p className='text-sm text-gray-600'>{item.description}</p>
+                            </div>
+
+                            {/* Image for the News Page */}
+                            <div className='flex-shrink-0'>
+                                <img src={item.image} alt="Image of News Page" 
+                                className='w-full object-cover' />
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
     </div>
   )
 }

5c936f8fd5a24dd97fb5db5f8d5167d1d73c6144
SadMark011 <97679783+SadMark011@users.noreply.github.com>
2024-11-16 13:34:46 +0530

    Finished Login and Register page UI Added React Hook Form to it for form data

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form"
+
+
 
 const Login = () => {
 
     const [message, setMessage] = useState("")
+
+    // Form Validation and Data handling using React Hook Form
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()       
+    
+    const onSubmit = (data) => console.log(data)
+
+    const handleGoogleSignIn = () => {
+        // Backend 
+    }
 
   return (
     <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
@@ -14,26 +31,36 @@ const Login = () => {
 
             <h2 className='text-xl font-semibold mb-4'>Please Login</h2>
 
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                {/* Email Input */}
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' 
                     htmlFor="email">Email</label>
-                    <input type="email" name='email' id='email' placeholder='Email Address'
-                    className='shawdow appearance-none border rounded w-full py-2 px-3 
-                    leading-tight focus:outline-none focus:shadow'/>
-                </div>
-                <div className='mb-4'>
-                    <label className='block text-gray-700 text-sm font-bold mb-2' 
-                    htmlFor="password">Password</label>
-                    <input type="password" name='password' id='password' placeholder='Enter Your Password'
+                    <input 
+                    {...register("email", { required: true })}
+                    type="email" name='email' id='email' placeholder='Email Address'
                     className='shawdow appearance-none border rounded w-full py-2 px-3 
                     leading-tight focus:outline-none focus:shadow'/>
                 </div>
 
+                {/* Password Input */}
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' 
+                    htmlFor="password">Password</label>
+                    <input 
+                    {...register("password", { required: true })}
+                    type="password" name='password' id='password' placeholder='Enter Your Password'
+                    className='shawdow appearance-none border rounded w-full py-2 px-3 
+                    leading-tight focus:outline-none focus:shadow'/>
+                </div>
+
+                {/* In case password or email is invalid */}
                 {
                     message && <p className='text-red-500 text-sm italic mb-3'>{message}</p>
                 }
 
+                {/* Login Button */}
                 <div> 
                     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 
                     rounded focus:outline-none'>
@@ -47,7 +74,9 @@ const Login = () => {
 
             {/* Google Sign In */}
             <div className='mt-3'>
-                <button className='w-full flex flex-wrap gap-1 items-center justify-center bg-secondary 
+                <button 
+                onClick={handleGoogleSignIn}
+                className='w-full flex flex-wrap gap-1 items-center justify-center bg-secondary 
                 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none'>
                     
                     <FaGoogle className='mr-2 mb-0.5'/> Sign In with Google
